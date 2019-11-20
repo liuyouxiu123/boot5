c6f9ff761163ade1080f46bae08e20f44a305ed6
liuchen_global <1099907221@qq.com>
2019-11-20 11:46:29 +0800

    陈哥第一次提交项目

---
+++ src/main/java/com/qianfeng/Boot5SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.qianfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot5SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot5SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qianfeng/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.qianfeng.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+   @RequestMapping("info")
+   public String info(){
+       return "hello hello";
+   }
+
+}
